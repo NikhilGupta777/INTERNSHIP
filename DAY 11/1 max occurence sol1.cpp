d0c18547d576bee7371dec83217060f900c7298e
Nikhil gupta <2022a1r090@mietjammu.in>
2024-07-20 15:47:25 +0530

    Add files via upload

---
+++ DAY 11/1 max occurence sol1.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+class Solution
+{
+
+public:
+    int findMaxConsecutiveOnes(vector<int> &nums)
+    {
+
+        int sum = 0, mSum = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+
+        {
+
+            if (nums[i] == 0)
+
+            {
+
+                sum = 0;
+            }
+
+            else
+            {
+
+                sum += 1;
+
+                if (sum > mSum)
+
+                    mSum = sum;
+            }
+        }
+
+        return mSum;
+    }
+};
+int main()
+{
+    Solution s;
+    vector<int> nums = {1, 1, 0, 1, 1, 1};
+    cout << s.findMaxConsecutiveOnes(nums);
+}
